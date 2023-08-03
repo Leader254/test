6fdc3e6855b012f673edf5dc62a6ddd7982366db
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-08-03 21:48:35 +0300

    still testing

--- test2/app.jsx
+++ test2/app.jsx
@@ -4,7 +4,14 @@ import { useState } from 'react'
 const app = () => {
     const [count, setCount] = useState(0)
     return (
-        <div>app</div>
+
+        <div className="counter">
+            <h1>Counter</h1>
+            <h2>{count}</h2>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
     )
 }
 
